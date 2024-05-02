c761c77346bbf2f46f4b8773de4fca7880009fcb
ibrahimaBah224 <159564053+ibrahimaBah224@users.noreply.github.com>
2024-05-02 15:41:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@
 
 (function() {
     "use strict";
-
-    /**
-     * Easy selector helper function
-     */
     const select = (el, all = false) => {
         el = el.trim()
         if (all) {
@@ -15,9 +11,6 @@
         }
     }
 
-    /**
-     * Easy event listener function
-     */
     const on = (type, el, listener, all = false) => {
         let selectEl = select(el, all)
         if (selectEl) {
@@ -29,16 +22,9 @@
         }
     }
 
-    /**
-     * Easy on scroll event listener 
-     */
     const onscroll = (el, listener) => {
         el.addEventListener('scroll', listener)
     }
-
-    /**
-     * Navbar links active state on scroll
-     */
     let navbarlinks = select('#navbar .scrollto', true)
     const navbarlinksActive = () => {
         let position = window.scrollY + 200
@@ -55,10 +41,6 @@
     }
     window.addEventListener('load', navbarlinksActive)
     onscroll(document, navbarlinksActive)
-
-    /**
-     * Scrolls to an element with header offset
-     */
     const scrollto = (el) => {
         let header = select('#header')
         let offset = header.offsetHeight
@@ -73,10 +55,6 @@
             behavior: 'smooth'
         })
     }
-
-    /**
-     * Toggle .header-scrolled class to #header when page is scrolled
-     */
     let selectHeader = select('#header')
     if (selectHeader) {
         const headerScrolled = () => {
@@ -90,34 +68,12 @@
         onscroll(document, headerScrolled)
     }
 
-    /**
-     * Back to top button
-     */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-        const toggleBacktotop = () => {
-            if (window.scrollY > 100) {
-                backtotop.classList.add('active')
-            } else {
-                backtotop.classList.remove('active')
-            }
-        }
-        window.addEventListener('load', toggleBacktotop)
-        onscroll(document, toggleBacktotop)
-    }
-
-    /**
-     * Mobile nav toggle
-     */
     on('click', '.mobile-nav-toggle', function(e) {
         select('#navbar').classList.toggle('navbar-mobile')
         this.classList.toggle('bi-list')
         this.classList.toggle('bi-x')
     })
 
-    /**
-     * Mobile nav dropdowns activate
-     */
     on('click', '.navbar .dropdown > a', function(e) {
         if (select('#navbar').classList.contains('navbar-mobile')) {
             e.preventDefault()
@@ -125,9 +81,6 @@
         }
     }, true)
 
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
     on('click', '.scrollto', function(e) {
         if (select(this.hash)) {
             e.preventDefault()
@@ -143,9 +96,6 @@
         }
     }, true)
 
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
     window.addEventListener('load', () => {
         if (window.location.hash) {
             if (select(window.location.hash)) {
@@ -154,9 +104,6 @@
         }
     });
 
-    /**
-     * Intro type effect
-     */
     const typed = select('.typed')
     if (typed) {
         let typed_strings = typed.getAttribute('data-typed-items')
@@ -170,61 +117,11 @@
         });
     }
 
-    /**
-     * Initiate portfolio lightbox 
-     */
-    const portfolioLightbox = GLightbox({
-        selector: '.portfolio-lightbox'
-    });
-
-    /**
-     * Testimonials slider
-     */
-    new Swiper('.testimonials-slider', {
-        speed: 600,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-        },
-        slidesPerView: 'auto',
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        }
-    });
-
-    /**
-     * Portfolio details slider
-     */
-    new Swiper('.portfolio-details-slider', {
-        speed: 400,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        }
-    });
-
-    /**
-     * Preloader
-     */
     let preloader = select('#preloader');
     if (preloader) {
         window.addEventListener('load', () => {
             preloader.remove()
         });
     }
-
-    /**
-     * Initiate Pure Counter 
-     */
-    new PureCounter();
 
 })()
